3f959a713ac3b91fcd820cec9421ea4820b6ddf6
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-13 01:23:48 +0300

    Reduced the regex match groups from 4 to 3
    from
    `(uint8_t)(("string"))(uint8_t)`
    to
    `(uint8_t)("string")(uint8_t)`
    
    Spaces are ignored

--- pattern.h
+++ pattern.h
@@ -6,7 +6,7 @@ int RET;
 regex_t REGEX;
 /* regex pattern */
 #define UINT8_T__PATTERN "(25[0-5]|2[0-4][0-4]|[0-1]?[0-9]?[0-9])"
-#define PATTERN "^"UINT8_T__PATTERN"[[:space:]]+(\"([^\"]+)\")[[:space:]]+"UINT8_T__PATTERN"$"
+#define PATTERN "^"UINT8_T__PATTERN"[[:space:]]+\"([^\"]+)\"[[:space:]]+"UINT8_T__PATTERN"$"
 
 
 #endif // PATTERN_H
