de46c21b00cbb6ac4f12b1e50251aeb17c2435c8
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-13 01:16:55 +0300

    Modified the regex pattern I ensured all the numbers are below 256 and they match 4 groups `(uint8_t)(("string"))(uint8_t)`  ^        ^^          ^  |        |\          |  1        2 3         4

--- pattern.h
+++ pattern.h
@@ -5,7 +5,8 @@
 int RET;
 regex_t REGEX;
 /* regex pattern */
-#define PATTERN "([0-9]+)[[:space:]]+\"([^\"]+)\"[[:space:]]+([0-9]+)"
+#define UINT8_T__PATTERN "(25[0-5]|2[0-4][0-4]|[0-1]?[0-9]?[0-9])"
+#define PATTERN "^"UINT8_T__PATTERN"[[:space:]]+(\"([^\"]+)\")[[:space:]]+"UINT8_T__PATTERN"$"
 
 
 #endif // PATTERN_H
