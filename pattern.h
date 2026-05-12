a42eac13997a3e185744f188343191fb36aa5d81
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-13 02:04:39 +0300

    Added an estracted strings structure for managing extraction
    this will contain
    - the number of strings
    - the length of each string
    - the data of each string
    this is for easier string handling

--- pattern.h
+++ pattern.h
@@ -6,7 +6,24 @@ int RET;
 regex_t REGEX;
 /* regex pattern */
 #define UINT8_T__PATTERN "(25[0-5]|2[0-4][0-4]|[0-1]?[0-9]?[0-9])"
-#define PATTERN "^"UINT8_T__PATTERN"[[:space:]]+\"([^\"]+)\"[[:space:]]+"UINT8_T__PATTERN"$"
+#define PATTERN "^[[:space:]]+"UINT8_T__PATTERN"[[:space:]]+\"([^\"]+)\"[[:space:]]+"UINT8_T__PATTERN"[[:space:]]+$"
+#define PATTERN_GROUPS 4
 
+/*
+matches == 4
+0 -> entire group
+1 -> uint8_t
+2 -> string
+3 -> uint8_t
+*/
 
+struct extracted_strings
+{
+    int number_of_groups;
+    int *length_of_strings;
+    char **strings;
+};
+
+struct extracted_strings *extract_data_from_string(char *s);
+void free_extracted_strings(struct extracted_strings *es);
 #endif // PATTERN_H
