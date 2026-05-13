b2fe7d640de4c7c1950316c67b5694d5defd51a6
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-13 21:21:57 +0300

    Fixed the arrow padding in the `'boot'` text

--- main.h
+++ main.h
@@ -8,8 +8,8 @@
 #include "pattern.h"
 
 #define BOOTING_SEQUENCE "$ > 9  \"Execution of write file syscall\" 0\n" \
-    "    ^          ^                           ^\n" \
-    "    |          |                           |\n" \
+    "    ^          ^                          ^\n" \
+    "    |          |                          |\n" \
     "    |          |                          This is the exitcode of the thread\n" \
     "    |          This is what to print after a job has been executed\n" \
     "    This is the burst time if the job in seconds\n" \
