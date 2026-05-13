820254cb5f5532d9946a52f66074f8344755197e
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-13 22:03:20 +0300

    Added a null check for free_extracted_strings

--- free_extracted_strings.c
+++ free_extracted_strings.c
@@ -2,7 +2,9 @@
 
 void free_extracted_strings(struct extracted_strings *es) /* \brief frees the es struct \deprecated if not written tests for*/
 {
-    for (size_t i = 0;i < es->number_of_groups;i++)
+    if (!es)
+        return;
+    for (size_t i = 0; i < es->number_of_groups; i++)
     {
         if (es->strings[i])
             free(es->strings[i]);
