377c2ac333eb70909c25e47185e8724be9202515
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-14 00:03:50 +0300

    Fixed the regex extraction if regex is not matched and the length of the string is greater

--- main_program.c
+++ main_program.c
@@ -8,8 +8,8 @@ int main_program(int argc, char **argv)
     while ((input = get_user_input()) && tolower(input[0]) != 'q')
     {
         struct extracted_strings *es = extract_data_from_string(input);
-        if (!es || strlen(input) > 0) {
-            printf("%s\n", input);
+        if (!es && strlen(input) > 0) {
+            printf("NO MATCH: %s\n", input);
             continue;
         }
 
