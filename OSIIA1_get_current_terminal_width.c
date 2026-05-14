bb5f6bacb29b86c54e09a986e14859148fbdc8b2
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-15 02:25:54 +0300

    Added a way to get the current window function

---
+++ OSIIA1_get_current_terminal_width.c
@@ -0,0 +1,4 @@
+#include "OSIIA1_terminal.h"
+
+void get_current_terminal_width(int *rows, int *cols)
+{}
---
+++ OSIIA1_terminal.h
@@ -0,0 +1,5 @@
+#if !defined(OSIIA1_TERMINAL_H)
+#define OSIIA1_TERMINAL_H
+void get_current_terminal_width(int *rows, int *cols);
+
+#endif // OSIIA1_TERMINAL_H
