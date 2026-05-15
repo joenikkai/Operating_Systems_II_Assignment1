2fe636fe802dddb46c22b1170a3855bac0c02db9
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-15 12:56:06 +0300

    Added buckets to manage processes

--- OSIIA1_threads.h
+++ OSIIA1_threads.h
@@ -9,5 +9,19 @@
  * - programs that are incoming
  * The program that is currently running does not need a bucket because it is already in the VM's CPU
 */
+#include <time.h>
 
+struct Bucket {
+    int job_id;
+    char *job_e_msg;
+    time_t time_burst_time;
+};
+
+extern struct Bucket *in_bucket; /* incoming processes */
+extern struct Bucket *out_bucket; /* completed processes */
+extern struct Bucket *sus_bucket; /* suspended but not finished processes */
+
+struct Bucket *in_bucket;
+struct Bucket *out_bucket;
+struct Bucket *sus_bucket;
 #endif // OSIIA1_THREADS_H
