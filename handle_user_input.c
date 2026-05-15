b4943301f82bf29ca5b9d43af7a8912b79185247
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-16 02:15:47 +0300

    Removed the while loop from the handle input so as to make it a thread

--- handle_user_input.c
+++ handle_user_input.c
@@ -5,31 +5,37 @@
 
 int handle_user_input(int argc, char **argv)
 {
-    char *input;
-    while ((input = get_user_input()) && strcmp(OSIIA1_strn_to_lower(input, (size_t)strlen(input)), "exit") != 0 && program_is_running)
-    {
-        if (strlen(input) <= 0)
-        {
-            continue;
-        }
-        struct extracted_strings *es = extract_data_from_string(input);
-        if (!es)
-        {
-            printf("command `%s' not found.\n", input);
-            continue;
-        }
+    char *input = get_user_input();
+    
+    if (strcmp(OSIIA1_strn_to_lower(input, (size_t)strlen(input)), "exit") == 0)
+        return 0;
+    /* --- */
 
-        /* make a new job */
-        struct Job *new_job = get_new_job(es);
-        if (!new_job)
-        {
-            printf("could not make a new job\n");
-            continue;
-        }
-        printf("arrival time: %zu | burst time: %hhu | exit message: %s | exit code: %hhu\n", new_job->arrival_time, new_job->burst, new_job->e_msg, new_job->e_code);
-        free_extracted_strings(es);
-        free(input);
+    if (strlen(input) <= 0)
+    {
+        return 0;
     }
+    
+    struct extracted_strings *es = extract_data_from_string(input);
+    if (!es)
+    {
+        printf("command `%s' not found.\n", input);
+        return 1;
+    }
+
+    /* make a new job */
+    struct Job *new_job = get_new_job(es);
+    if (!new_job)
+    {
+        printf("could not make a new job\n");
+        return 2;
+    }
+    printf("arrival time: %zu | burst time: %hhu | exit message: %s | exit code: %hhu\n", new_job->arrival_time, new_job->burst, new_job->e_msg, new_job->e_code);
+    free_extracted_strings(es);
+    free(input);
+    
     if (input)
         free(input);
+    /* --- */
+    return 0;
 }
