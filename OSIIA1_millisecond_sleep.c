bcc6a92561d8e89d8733c3ebceddfcac5c724ffd
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-15 03:49:36 +0300

    compressed `OSIIA1_millisecond_sleep`

--- OSIIA1_millisecond_sleep.c
+++ OSIIA1_millisecond_sleep.c
@@ -1,28 +1,20 @@
 #include "timer.h"
 
+static inline void OSIIA1_millisecond_sleep(time_t time_quanta)
+{
 
+/**
+ * \brief Sleep for a specified number of milliseconds.
+ * \param milliseconds Time to sleep in milliseconds.
+ */
 #if defined(__WIN32) /* platform */
-/**
- * \brief Sleep for a specified number of milliseconds.
- * \param milliseconds Time to sleep in milliseconds.
- */
-
-static inline void OSIIA1_millisecond_sleep(time_t time_quanta)
-{
     Sleep(time_quanta);
-}
 #elif defined(__unix__)
-/**
- * \brief Sleep for a specified number of milliseconds.
- * \param milliseconds Time to sleep in milliseconds.
- */
-static inline void OSIIA1_millisecond_sleep(time_t time_quanta)
-{
     struct timespec ts;
     ts.tv_sec = time_quanta/1000;
     ts.tv_nsec = (time_quanta % 1000) * 1000000;
     nanosleep(&ts, NULL);
-}
 #else
 #error PLATFORM_COMPATIBILITY_ERROR
 #endif /* platform */
+}
