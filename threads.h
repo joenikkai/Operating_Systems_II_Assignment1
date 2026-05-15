ff5dc99b9c8c8d8601d66b35285016422e6ce7d6
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-15 11:12:16 +0300

    Made plans

--- threads.h
+++ threads.h
@@ -1,3 +1,13 @@
 #if !defined(THREADS_H)
 #define THREADS_H
+
+/**
+ * constantly update window size.
+ * manage the 3 buckets
+ * - programs that have already run
+ * - program that have been suspended
+ * - programs that are incoming
+ * The program that is currently running does not need a bucket because it is already in the VM's CPU
+*/
+
 #endif // THREADS_H
