c061707d44777b236831d9e999045b2279331f2f
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-16 01:41:06 +0300

    made a function dedicated only to userinput

--- main_program.c
+++ main_program.c
@@ -1,35 +1,15 @@
 #include "input.h"
-#include "pattern.h"
-#include "jobs.h"
 #include "timer.h"
+#include "jobs.h"
 #include "OSIIA1_threads.h"
 
 int main_program(int argc, char **argv)
 {
-    char *input;
-    while ((input = get_user_input()) && strcmp(OSIIA1_strn_to_lower(input, (size_t)strlen(input)), "exit") != 0)
+    int retval = 0;
+    while (program_is_running)
     {
-        if (strlen(input) <= 0)
-        {
-            continue;
-        }
-            struct extracted_strings *es = extract_data_from_string(input);
-        if (!es) {
-            printf("command `%s' not found.\n", input);
-            continue;
-        }
-
-        /* make a new job */
-        struct Job *new_job = get_new_job(es);
-        if (!new_job) {
-            printf("could not make a new job\n");
-            continue;
-        }
-        printf("arrival time: %zu | burst time: %hhu | exit message: %s | exit code: %hhu\n",new_job->arrival_time,new_job->burst,new_job->e_msg,new_job->e_code);
-        free_extracted_strings(es);
-        free(input);
+        retval = handle_user_input(argc, argv);
     }
-    if (input)
-        free(input);
-    return 0;
+
+    return retval;
 }
