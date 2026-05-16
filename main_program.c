05cc1e516d393865e7e98accc5c507490948549e
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-17 02:08:03 +0300

    started using threads in the program.

--- main_program.c
+++ main_program.c
@@ -5,13 +5,35 @@
 
 int main_program(int argc, char **argv)
 {
-    OSIIA1_thread_t cpu_thread;
+    /* cpu thread */
+    OSIIA1_thread_t execute_job_instance_thread;
+    int execute_job_instance_thread_return_value = 0;
 
+    /* handle user input thread */
+    OSIIA1_thread_t handle_user_input_thread;
+    int handle_user_input_thread_return_value = 0;
     int retval = 0;
     while (PROGRAM_IS_RUNNING)
     {
-        handle_user_input();
+        /* start threads */
+        /* handle user input  */
+        execute_job_instance_thread_return_value = pthread_create(&execute_job_instance_thread,NULL,&execute_job_instance,NULL);
+        if (execute_job_instance_thread_return_value!=0)
+        {
+            perror("pthread_create");
+            return 1;
+        }
+        /* handle user input  */
+        handle_user_input_thread_return_value = pthread_create(&handle_user_input_thread, NULL, &handle_user_input, NULL);
+        if (handle_user_input_thread_return_value != 0)
+        {
+            perror("pthread_create");
+            return 1;
+        }
 
+        /* --- join all threads --- */
+        pthread_join(execute_job_instance_thread, NULL); /* job instance thread */
+        pthread_join(handle_user_input_thread, NULL); /* handle input thread */
     }
 
     return retval;
