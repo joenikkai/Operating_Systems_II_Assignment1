c30b96c0acd972ed56a45cb4d2a16923ca420d3d
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-17 02:27:58 +0300

    removed unnecessary duplication

--- OSIIA1_trim_leading_and_trailing_whitespace.c
+++ OSIIA1_trim_leading_and_trailing_whitespace.c
@@ -2,15 +2,13 @@
 
 char *OSIIA1_trim_leading_and_trailing_whitespace(char *s)
 {
-    char *str = strdup(s);
-
-    char *start = str;
-    char *end = str + strlen(str) - 1;
+    char *start = s;
+    char *end = s + strlen(s) - 1;
     while (isspace((unsigned char)*start))
         start++;
     while (end > start && isspace((unsigned char)*end))
         end--;
     size_t len = end - start + 1;
-    str[len] = '\0';
-    return str;
+    s[len] = '\0';
+    return s;
 }
