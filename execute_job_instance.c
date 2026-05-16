f97f410ecdbcfcb12cc199089925e016aa70f26f
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-17 02:47:10 +0300

    added while loop in the cpu execution cycle

--- execute_job_instance.c
+++ execute_job_instance.c
@@ -1,6 +1,13 @@
 #include "OSIIA1_threads.h"
+#include "timer.h"
 
 void *execute_job_instance(void *arg)
 {
+    while (PROGRAM_IS_RUNNING)
+    {
+        OSIIA1_sleep(TIME_QUANTA);
+        printf("\nexecution cycle");
+    }
+
     return NULL;
 }
