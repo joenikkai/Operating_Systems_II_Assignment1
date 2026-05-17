a00cde8f90eeaee4635238ce9914b793b7cd687e
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-18 01:41:20 +0300

    Optimize boot time and add skip functionality via any key press

--- OSIIA1_play_boot_sequence.c
+++ OSIIA1_play_boot_sequence.c
@@ -8,7 +8,7 @@ void OSIIA1_play_boot_sequence()
     curs_set(0);
 
     const char *bs[] = BOOTING_SEQUENCE;
-    int cyc_t = 667;                        /* cycle time */
+    int cyc_t = 150;                        /* cycle time - faster for reasonable boot */
     int nbs = (sizeof(bs) / sizeof(bs[0])); /* number of cycles */
 
     WINDOW *b_logger_d_w = newwin(WINDOW_HEIGHT - 3, WINDOW_WIDTH, 0, 0); /* boot log display window */
@@ -38,8 +38,12 @@ void OSIIA1_play_boot_sequence()
     box(b_blinker_d_w, 0, 0);
     refresh();
 
+    nodelay(stdscr, TRUE); // Enable non-blocking input for skipping
+
     for (int cyc = 0; cyc < nbs + 5; cyc++)
     {
+        if (wgetch(stdscr) != ERR) // Skip if any key is pressed
+            break;
 
         OSIIA1_play_boot_sequence_log(&b_logger_d_w, cyc, bs, nbs);
         OSIIA1_play_boot_sequence_loader(&b_loading_bar_d_w, cyc, nbs);
@@ -47,7 +51,9 @@ void OSIIA1_play_boot_sequence()
         OSIIA1_millisecond_sleep(cyc_t);
     }
 
-    OSIIA1_sleep(2);
+    nodelay(stdscr, FALSE); // Disable non-blocking input
+
+    OSIIA1_millisecond_sleep(200); // Small pause instead of 2 seconds
     delwin(b_logger_d_w);
     delwin(b_loading_bar_d_w);
     delwin(b_blinker_d_w);
