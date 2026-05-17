812199af8edae9c2aa3ae74cf8fed667aa2884b2
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-17 20:30:21 +0300

    added a controlled print in each cycle

--- execute_job_instance.c
+++ execute_job_instance.c
@@ -4,10 +4,14 @@
 
 void *execute_job_instance(void *arg)
 {
+    int line = 0;
+    int inner_height = (getmaxy(CPU_EXEC_LOG_WIN) - 2);
     while (PROGRAM_IS_RUNNING)
     {
+        if (line >= inner_height - 1)
+            line = 0;
         OSIIA1_sleep(TIME_QUANTA);
-        wprintw(CPU_EXEC_LOG_WIN, "\nexecution cycle");
+        mvwprintw(CPU_EXEC_LOG_WIN,line++,1, "execution cycle");
     }
 
     return NULL;
