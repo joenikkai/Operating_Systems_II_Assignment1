c8f9dcb0cd90e2eb78370983872d1ab4b08dadbe
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-17 10:25:53 +0300

    replaced manual terminal queries in favor of ncurses

--- OSIIA1_get_current_terminal_width.c
+++ OSIIA1_get_current_terminal_width.c
@@ -1,16 +1,7 @@
 #include "OSIIA1_terminal.h"
-#include <stdio.h>
-#include <stdlib.h>
+
+
 void get_current_terminal_width(int *rows, int *cols)
 {
-    struct winsize w;
-    if (ioctl(STDOUT_FILENO, TIOCGWINSZ,&w) == 0)
-    {
-        *rows = w.ws_row;
-        *cols = w.ws_col;
-    }
-    else
-    {
-        *rows = *cols = 0;
-    }
+    getmaxyx(stdscr, *cols, *rows);
 }
--- OSIIA1_terminal.h
+++ OSIIA1_terminal.h
@@ -1,9 +1,7 @@
 #if !defined(OSIIA1_TERMINAL_H)
 #define OSIIA1_TERMINAL_H
 
-#include <sys/ioctl.h>
-#include <sys/ioctl.h>
-#include <unistd.h>
+#include <ncurses/ncurses.h>
 
 void get_current_terminal_width(int *rows, int *cols);
 void OSIIA1_print_horirontal_line(int *len, char *c,int n);
